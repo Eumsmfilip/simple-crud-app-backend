495892a522f4630b288a54958483efb6ca266d1f
Filipe Rocha <filiperochasantos4@gmail.com>
2024-12-23 07:52:29 -0300

    refactor: JSON responses

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -25,8 +25,8 @@ const getProductById = async (req, res) => {
 const createProduct = async (req, res) => {
   try {
     const product = await Product.create(req.body);
-    res.status(200).json(product);
     console.log(product);
+    res.status(200).json({ product });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -40,7 +40,7 @@ const updateProduct = async (req, res) => {
       return res.status(404).json({ message: "Product not found." });
     }
     const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
+    res.status(200).json({updatedProduct});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
